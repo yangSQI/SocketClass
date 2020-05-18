da6e1f2467544f18ba3515f6d338f4ce29f837a3
yangSQI <844318220@qq.com>
2020-05-18 19:11:50 +0800

    跨平台SOCKET头文件声明

--- SocketClass/SocketBase.h
+++ SocketClass/SocketBase.h
@@ -1,6 +1,18 @@
 #ifndef __SOCKETBASE_H__
 #define __SOCKETBASE_H__
-
+	#ifdef _WIN32
+		#define WIN32_LEAN_AND_MEAN
+		#include <WinSock2.h>
+		#include <windows.h>
+		#pragma comment(lib, "ws2_32.lib")
+	#else
+		#include <sys/types.h>
+		#include <sys/socket.h>
+		#include <netinet/in.h>
+		#include <arpa/inet.h>
+		#include <unistd.h>
+		#include <fcntl.h>
+	#endif
 class SocketBase
 {
 	
